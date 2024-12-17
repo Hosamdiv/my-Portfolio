07f393af6ea9009a16f855a126660dc221dc5571
hosam rda <rdhosam712@gmail.com>
2024-12-17 06:32:25 +0200

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,82 @@
+import {
+  FaFacebookF,
+  FaLinkedinIn,
+  FaTelegramPlane,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { Button } from "../components/ui/button";
+import { Link } from "react-router-dom";
+import { CiDesktopMouse2 } from "react-icons/ci";
+import { IoIosArrowRoundDown } from "react-icons/io";
+import { RiGithubLine } from "react-icons/ri";
+import { SiHandshakeProtocol } from "react-icons/si";
+
 const HomePage = () => {
   return (
-    <div>
-      {/* <div>
-        <h1>hosam Reda</h1>
-        <p>
-          My beginning was in the field of front-end, since the beginning of the
-          youth stage and I love programming a lot and have a great passion for
-          it and learn it very quickly, and since 2021 .
-        </p>
-      </div> */}
+    <div className="md:pt-52 items-center ml-5 ">
+      <div className="flex items-center flex-col w-[90%] m-auto md:flex-row">
+        <div className="my-44 space-y-5 md:w-[70%]">
+          <div className="space-y-2">
+            <h1 className="flex text-5xl font-bold">
+              Hosam Reda
+              <span className="ml-2">
+                <SiHandshakeProtocol />
+              </span>
+            </h1>
+            <p>________FrontEnd Developer</p>
+          </div>
+          <div>
+            <p className="max-w-[90%] opacity-50">
+              My beginning was in the field of front-end, since the beginning of
+              the <br/> youth stage and I love programming a lot and have a great
+              passion for it and learn it very quickly, and since 2021...
+            </p>
+          </div>
+          <div className="flex items-center  space-x-4  ">
+            <Button className="rounded-xl ">
+              <FaTelegramPlane /> Say Hello
+            </Button>
+            <Button className="rounded-xl bg-[rgb(37,211,102)] text-white">
+              <FaWhatsapp /> WhatsApp
+            </Button>
+
+            <div className="flex text-2xl space-x-4 ml-10 ">
+              <div className="pr-2">
+                <Link to="https://www.facebook.com/profile.php?id=100026338921350&locale=ar_AR">
+                  <FaFacebookF />
+                </Link>
+              </div>
+              <div className="pr-2">
+                <Link to="https://www.linkedin.com/in/hosam-reda-5758282a2">
+                  <FaLinkedinIn />
+                </Link>
+              </div>
+              <div className="pr-2">
+                <Link to="https://github.com/Hosamdiv">
+                  <RiGithubLine />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <img
+            className="classImage"
+            src="/my image/name.jpeg"
+            width={400}
+            alt="my name"
+            
+          />
+        </div>
+      </div>
+      <div>
+        <button className="flex m-auto items-center w-fit text-1xl space-x-1 py-12">
+          <CiDesktopMouse2 className="text-4xl" />
+          Scroll down
+          <IoIosArrowRoundDown className="text-3xl animate-bounce text-[rgb(37,211,102)]" />
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -6,10 +6,10 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <nav className="shadow-md w-full fixed top-0 left-0">
-      <div className="md:flex justify-between items-center py-4 md:px-10 px-7 ">
+    <nav className="shadow-md w-full fixed top-0 left-0 h-24 z-10">
+      <div className="md:flex justify-around items-center py-4 md:px-10 px-7 ">
         <div className="text-2xl font-bold cursor-pointer font-[Poppins]">
-          <h1 className="">Hosam</h1>
+          <img src="/my image/name.jpeg" width={60} className="rounded-[50%]" alt="" />
         </div>
         <div
           onClick={() => setOpen(!open)}
@@ -20,20 +20,17 @@ const NavBar = () => {
           {open ? <CiMenuFries /> : <IoMdClose />}
         </div>
         <ul
-          className={`md:flex md:items-center
-         md:pb-0 pb-12 absolute md:static md:z-[-1] left-0 w-full
-         md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in
-        ${
-          open ? "top-20 opacity-100" : "top-[-490px] md:opacity-100 opacity-0"
-        }
+          className={`md:flex md:items-center 
+         md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1]
+          left-0 w-full md:w-auto md:pl-0 pl-9 transition-all 
+         duration-500 ease-in
+        ${open ? "top-20 opacity-100 bg-white text-black" : "top-[-490px] md:opacity-100 opacity-0"}
           
          `}
         >
-          {nav.map((ele) => (
-            <li key={ele.name} className="md:ml-8 text-xl md:my-0 my-7">
-              <NavLink className="" to={ele.link}>
-                {ele.name}
-              </NavLink>
+          {nav.map((item) => (
+            <li key={item.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <NavLink to={item.link}>{item.name}</NavLink>
             </li>
           ))}
         </ul>
@@ -50,5 +47,4 @@ const nav = [
   { name: "Skills", link: "/" },
   { name: "Services", link: "/" },
   { name: "Portfolio", link: "/" },
-  { name: "Contact", link: "/" },
 ];
